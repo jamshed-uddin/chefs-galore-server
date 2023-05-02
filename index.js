4f2339b374e23e65dadca1d0b7a026b2035094fb
Jami <jamsheduddin03@gmail.com>
2023-05-02 20:37:30 +0600

    food and review data for extra 2 section

--- index.js
+++ index.js
@@ -1,17 +1,21 @@
 const express = require("express");
 const chefs = require("./data/chefs.json");
+const recipes = require("./data/recipes.json");
 const cors = require("cors");
 const app = express();
 const port = 4000;
 
 app.use(cors());
 
-app.get("/", (req, res) => {
+app.get("", (req, res) => {
   res.send({ message: "this server is running" });
 });
 
 app.get("/chefs", (req, res) => {
-  res.send(services);
+  res.send(chefs);
+});
+app.get("/recipes", (req, res) => {
+  res.send(recipes);
 });
 
 app.listen(port, () => {
